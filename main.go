65b4665e304d3692b19470736f4abcdf7b697b62
Ankur Srivastava <ankur.srivastava@recogizer.com>
2017-03-05 22:29:27 +0100

    read config files

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	flag "github.com/spf13/pflag"
+)
+
+var configName string
+
+func init() {
+	flag.StringVar(&configName, "config", "config.yaml", "path to a configuration file")
+}
 
 func main() {
-    fmt.Println("Hello World")
+	flag.Parse()
+	fmt.Println("config file is:", configName)
 }
